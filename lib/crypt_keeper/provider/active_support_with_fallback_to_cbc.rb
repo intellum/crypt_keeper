d55eaa97dd64bd43a7373a67d5596abd2910a893
Konstantin Rudy <krudy@intellum.com>
2025-04-08 10:01:54 +0200

    MAN-5899 Fixed custom encryptor loading issue

--- lib/crypt_keeper/provider/active_support_with_fallback_to_cbc.rb
+++ lib/crypt_keeper/provider/active_support_with_fallback_to_cbc.rb
@@ -3,7 +3,7 @@ require 'active_support/message_encryptor'
 module CryptKeeper
   module Provider
     # Extends base active_support provider with fallback to old encryption rules to be able to read old encrypted values
-    class ActiveSupportWithFallbackToCbc < ActiveSupport
+    class ActiveSupportWithFallbackToCbc < Provider::ActiveSupport
       def initialize(options = {})
         super
         encryptor.rotate(cipher: 'aes-256-cbc')
